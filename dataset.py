a23a03c38f8be8ff6236479a344b87e4a302ef3e
Iwanaga Jiro <pseudo.finite@gmail.com>
2016-07-12 23:26:54 +0900

    Refactoring cf and add middle size dataset

--- dataset.py
+++ dataset.py
@@ -17,13 +17,39 @@ no=2
 ,[1,3,2,0]
 '''
 
+'''
+no=3
+中規模データの作成
+'''
+
+def createDummyData(N=2000, M=400, K=20, R=5, seed=1):
+    import random
+    random.seed(seed)
+    
+    # user, item 生成
+    N_len = len(str(N))
+    M_len = len(str(M))
+    Us = [("u%0"+ str(N_len) +"d") % i for i in range(1,N+1)]
+    Is = [("i%0"+ str(M_len) +"d") % i for i in range(1,M+1)]
+
+    # user * item -> rating 生成
+    U2I2Value = {}
+    for u in Us:
+        U2I2Value[u]  = {}
+        m = random.randint(1,K)
+        tmpIs = random.sample(Is, m)
+        for i in tmpIs:
+            U2I2Value[u][i] = random.randint(1, R)
+    return Us, Is, U2I2Value
+
+
 class dataset:
     def __init__(self, no):
         
         self.Us = []         # user list
         self.Is = []         # item list
-        self.U2I2Rating = {} # user's rating for an item(sparse)
-        self.IRss = []       # user's item rating list(dense)
+        self.U2I2Rating = {} # [sparse] user の item に対する rating
+        self.Rss = []       # [dense]  rating list の list
 
         if no==1:
             # (user,item)=5*4 の rating dictionary を作成
@@ -44,20 +70,20 @@ class dataset:
             self.U2I2Rating["u2"]["i2"] = 1;  self.U2I2Rating["u2"]["i3"] = 3;
             self.U2I2Rating["u3"]["i1"] = 2;  self.U2I2Rating["u3"]["i2"] = 1;  self.U2I2Rating["u3"]["i3"] = 3;  self.U2I2Rating["u3"]["i4"] = 1
             self.U2I2Rating["u4"]["i1"] = 1;  self.U2I2Rating["u4"]["i2"] = 3;  self.U2I2Rating["u4"]["i3"] = 2;  
+        elif no==3:
+            self.Us, self.Is, self.U2I2Rating = createDummyData()
         else:
             assert False
             
-
-
-        # rating list を作成
+        # rating list の list を作成
         for user in self.Us:
             IRs = []
             for item in self.Is:
                 rating = self.U2I2Rating[user][item] if item in self.U2I2Rating[user] else 0
                 IRs.append(rating)
-            self.IRss.append(IRs)
-
+            self.Rss.append(IRs)
         return
+
 
     def __str__(self):
         Us_s = "user:" + " ".join(self.Us) + "\n"
@@ -68,11 +94,11 @@ class dataset:
             IRs = [(item, str(self.U2I2Rating[user][item])) for item in self.U2I2Rating[user]]
             U2I2R_s += user + ": " + "".join(["({},{}) ".format(i,r) for i,r in IRs]) + "\n"
 
-        IRss_s = ""
-        for IRs in self.IRss:
-            IRss_s += " ".join(map(str, IRs)) + "\n"
+        Rss_s = ""
+        for IRs in self.Rss:
+            Rss_s += " ".join(map(str, IRs)) + "\n"
 
-        return "".join([Us_s, Is_s, U2I2R_s, IRss_s])
+        return "".join([Us_s, Is_s, U2I2R_s, Rss_s])
 
 
 
@@ -84,4 +110,8 @@ if __name__ == "__main__":
     no = 2
     data = dataset(no)
     print(data)
+
+    no = 3
+    data = dataset(no)
+    # print(data)
     
