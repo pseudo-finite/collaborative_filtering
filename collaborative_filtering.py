d2c471c1e607f5baf59eab6261d32f01b660f74a
Iwanaga Jiro <pseudo.finite@gmail.com>
2016-07-13 14:51:15 +0900

    change cf class __init__

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -1,49 +1,46 @@
 # -*- coding:utf-8 -*-
 
-import sys
 import math
-import dataset
 
-# def pearson_correlation(Is1, Is2, I2Rating1, I2Rating2):
-#     if len(Is1)<=1 or len(Is2)<=1:
-#         sim = 0
-#     else:
-#         Is12 = Is1 & Is2
-#         if len(Is12) <= 1:
-#             sim = 0
-#         else:
-#             Ratings1 = [I2Rating1[item] for item in Is12]
-#             Ratings2 = [I2Rating2[item] for item in Is12]
-#             avg1 = 1.0 * sum(Ratings1) / len(Ratings1)
-#             avg2 = 1.0 * sum(Ratings2) / len(Ratings2)
-#             X = sum([(rating-avg1)*(rating-avg1) for rating in Ratings1])
-#             Y = sum([(rating-avg2)*(rating-avg2) for rating in Ratings2])
-#             if X==0 or Y==0:
-#                 sim = 0
-#             else:
-#                 XY = sum([(Ratings1[i]-avg1)*(Ratings2[i]-avg2) for i in range(len(Ratings1))])
-#                 sim = XY / math.sqrt(X*Y)
-#     return sim
+def pearson_correlation(Is1, Is2, I2Rating1, I2Rating2):
+    if len(Is1)<=1 or len(Is2)<=1:
+        sim = 0
+    else:
+        Is12 = Is1 & Is2
+        if len(Is12) <= 1:
+            sim = 0
+        else:
+            Ratings1 = [I2Rating1[item] for item in Is12]
+            Ratings2 = [I2Rating2[item] for item in Is12]
+            avg1 = 1.0 * sum(Ratings1) / len(Ratings1)
+            avg2 = 1.0 * sum(Ratings2) / len(Ratings2)
+            X = sum([(rating-avg1)*(rating-avg1) for rating in Ratings1])
+            Y = sum([(rating-avg2)*(rating-avg2) for rating in Ratings2])
+            if X==0 or Y==0:
+                sim = 0
+            else:
+                XY = sum([(Ratings1[i]-avg1)*(Ratings2[i]-avg2) for i in range(len(Ratings1))])
+                sim = XY / math.sqrt(X*Y)
+    return sim
 
-# def cosine_similarity(I2Rating1, I2Rating2):
-#     XY = sum([rating1*I2Rating2[item] for item,rating1 in I2Rating1.items() if item in I2Rating2])
-#     if XY == 0:
-#         sim = 0
-#     else:
-#         X = sum([rating*rating for rating in I2Rating1.values()])
-#         Y = sum([rating*rating for rating in I2Rating2.values()])
-#         sim = XY / math.sqrt(X*Y)
-#     return sim
+def cosine_similarity(I2Rating1, I2Rating2):
+    XY = sum([rating1*I2Rating2[item] for item,rating1 in I2Rating1.items() if item in I2Rating2])
+    if XY == 0:
+        sim = 0
+    else:
+        X = sum([rating*rating for rating in I2Rating1.values()])
+        Y = sum([rating*rating for rating in I2Rating2.values()])
+        sim = XY / math.sqrt(X*Y)
+    return sim
 
 
 class cf:
-    def __init__(self, Us, Is, U2I2Rating, sim_func_name):
+    def __init__(self, Us, Is, U2I2Rating):
         self.Us = []         # user list
         self.Is = []         # item list
         self.U2I2Rating = {} # user の item に対する rating
         self.I2Us = {}       # item を評価した user list
         self.U2Is = {}       # user が評価した item set
-        self.U2U2Sim = {}    # user と user の similarity 
 
         # データのセット
         self.Us = Us
@@ -53,7 +50,6 @@ class cf:
         # データの前処理・計算
         self.setI2Us()
         self.setU2Is() 
-        self.setU2U2Sim(sim_func_name)
         return
 
     def setU2Is(self):
@@ -69,27 +65,6 @@ class cf:
             for item in Is:
                 self.I2Us[item].add(user)
         return 
-
-
-    def setU2U2Sim(self, sim_func_name):
-        for user1 in self.Us:
-            for user2 in self.Us:
-                self.U2U2Sim.setdefault(user1, {})
-                self.U2U2Sim.setdefault(user2, {})
-                if user1 >= user2:continue
-
-                # calc similarity
-                if sim_func_name == "pearson":
-                    # sim = pearson_correlation(self.U2Is[user1], self.U2Is[user2], self.U2I2Rating[user1], self.U2I2Rating[user2])
-                    sim = self.pearson_correlation(user1, user2)
-                elif sim_func_name == "cosine":
-                    # sim = cosine_similarity(self.U2I2Rating[user1], self.U2I2Rating[user2])
-                    sim = self.cosine_similarity(user1, user2)
-                
-                if sim != 0:
-                    self.U2U2Sim[user1][user2] = sim
-                    self.U2U2Sim[user2][user1] = sim
-        return
 
     def pearson_correlation(self, user1, user2):
         Is1 = self.U2Is[user1]
@@ -127,12 +102,16 @@ class cf:
             sim = XY / math.sqrt(X*Y)
         return sim
 
-    def calc_score(self, user1, item):
-        U2Sim = self.U2U2Sim[user1]
+    def calc_score(self, user1, item, sim_func_name):
         x = 0.0
         y = 0.0
         for user2 in self.I2Us[item]:
-            sim = U2Sim[user2] if user2 in U2Sim else 0
+            if sim_func_name == "pearson":
+                # sim = pearson_correlation(self.U2Is[user1], self.U2Is[user2], self.U2I2Rating[user1], self.U2I2Rating[user2])
+                sim = self.pearson_correlation(user1, user2)
+            elif sim_func_name == "cosine":
+                # sim = cosine_similarity(self.U2I2Rating[user1], self.U2I2Rating[user2])
+                sim = self.cosine_similarity(user1, user2)
             if sim==0:continue
             Is12 = self.U2Is[user1] & self.U2Is[user2]
             Ratings2 = [self.U2I2Rating[user2][item] for item in Is12]
@@ -150,6 +129,9 @@ class cf:
 
         
 if __name__ == "__main__":
+    import sys
+    import dataset
+
     print('*** begin ***');sys.stdout.flush()
 
     print('=== set sim_func ===');sys.stdout.flush()
@@ -164,7 +146,7 @@ if __name__ == "__main__":
 
     
     print('=== create CF model ===');sys.stdout.flush()
-    cf_model = cf(data.Us, data.Is, data.U2I2Rating, sim_func_name)
+    cf_model = cf(data.Us, data.Is, data.U2I2Rating)
     # print("U2Is :", cf_model.U2Is)
     # print("I2Us :", cf_model.I2Us)
     # print("U2Avg:", cf_model.U2Avg)
@@ -175,7 +157,7 @@ if __name__ == "__main__":
     for user in cf_model.Us:
         # print(user, "-"*50)
         for item in cf_model.Is:
-            score = cf_model.calc_score(user, item)
+            score = cf_model.calc_score(user, item, sim_func_name)
             # print("  ", item, score)
 
     print('***  end  ***');sys.stdout.flush()
