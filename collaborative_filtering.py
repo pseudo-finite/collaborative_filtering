3b1985d7b00cce19be7300e8012d78edf8504dc6
Iwanaga Jiro <pseudo.finite@gmail.com>
2016-07-14 19:16:15 +0900

    add test_code(nose)

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -134,16 +134,183 @@ class cf:
         RecIs = [item for item,score in I2Score[:N]]
         return RecIs
 
+
+
+### test code ##################################################
+
+### pearson correlation test
+def test_pearson_correlation1():
+    from nose.tools import eq_
+    I2Rating1 = {"i1":1}
+    I2Rating2 = {"i1":1, "i2":2}
+    eq_(pearson_correlation(I2Rating1, I2Rating2), 0.0)
+    return
+
+def test_pearson_correlation2():
+    from nose.tools import eq_
+    I2Rating1 = {"i1":1, "i2":2}
+    I2Rating2 = {"i1":1}
+    eq_(pearson_correlation(I2Rating1, I2Rating2), 0.0)
+    return
+
+def test_pearson_correlation3():
+    from nose.tools import eq_
+    I2Rating1 = {"i1":1, "i2":2}
+    I2Rating2 = {        "i2":2, "i3":3}
+    eq_(pearson_correlation(I2Rating1, I2Rating2), 0.0)
+    return
+
+def test_pearson_correlation4():
+    from nose.tools import eq_
+    I2Rating1 = {"i1":1, "i2":2, "i3":3}
+    I2Rating2 = {        "i2":2, "i3":3, "i4":4}
+    eq_(pearson_correlation(I2Rating1, I2Rating2), 1.0)
+    return
+
+def test_pearson_correlation5():
+    from nose.tools import eq_
+    I2Rating1 = {"i1":1, "i2":2, "i3":3}
+    I2Rating2 = {        "i2":3, "i3":2, "i4":4}
+    eq_(pearson_correlation(I2Rating1, I2Rating2), -1.0)
+    return
+
+def test_pearson_correlation6():
+    from nose.tools import eq_
+    I2Rating1 = {"i1":4, "i2":3, "i3":2, "i4":6, "i5":5}
+    I2Rating2 = {"i1":6, "i2":8, "i3":12,"i4":10,"i5":4}
+    eq_(pearson_correlation(I2Rating1, I2Rating2), -0.4)
+    return
+
+def test_pearson_correlation7():
+    from nose.tools import eq_
+    I2Rating1 = {"i0":1, "i1":4, "i2":3, "i3":2, "i4":6, "i5":5}
+    I2Rating2 = {        "i1":6, "i2":8, "i3":12,"i4":10,"i5":4, "i6":1}
+    eq_(pearson_correlation(I2Rating1, I2Rating2), -0.4)
+    return
+
+
+### cosine similarity test
+def test_cosine_similarity1():
+    from nose.tools import eq_
+    I2Rating1 = {"i1":1}
+    I2Rating2 = {"i1":1}
+    eq_(cosine_similarity(I2Rating1, I2Rating2), 1.0)
+    return
+
+def test_cosine_similarity2():
+    from nose.tools import eq_
+    I2Rating1 = {"i1":1, "i2":1}
+    I2Rating2 = {"i1":1, "i2":1}
+    eq_(cosine_similarity(I2Rating1, I2Rating2), 1.0)
+    return
+
+def test_cosine_similarity3():
+    from nose.tools import eq_
+    I2Rating1 = {"i1":1}
+    I2Rating2 = {        "i2":1}
+    eq_(cosine_similarity(I2Rating1, I2Rating2), 0.0)
+    return
+
+def test_cosine_similarity4():
+    from nose.tools import eq_
+    I2Rating1 = {"i1":1, "i2":1}
+    I2Rating2 = {                "i3":1, "i4":1}
+    eq_(cosine_similarity(I2Rating1, I2Rating2), 0.0)
+    return
+
+def test_cosine_similarity5():
+    from nose.tools import eq_
+    import math
+    I2Rating1 = {"i1":1, "i2":1}
+    I2Rating2 = {"i1":1}
+    eq_(cosine_similarity(I2Rating1, I2Rating2), 1.0/math.sqrt(2))
+    return
+
+def test_cosine_similarity5():
+    from nose.tools import eq_
+    import math
+    I2Rating1 = {"i1":1, "i2":7}
+    I2Rating2 = {"i1":5, "i2":-3}
+    eq_(cosine_similarity(I2Rating1, I2Rating2), -16.0 / (math.sqrt(50)*math.sqrt(34)))
+    return
+
+def test_cosine_similarity6():
+    from nose.tools import eq_
+    import math
+    I2Rating1 = {"i1":3, "i2":-5,"i3":6}
+    I2Rating2 = {"i1":2, "i2":4, "i3":-1}
+    eq_(cosine_similarity(I2Rating1, I2Rating2), -20.0 / (math.sqrt(70)*math.sqrt(21)))
+    return
+
+### recommendation test
+def test_pearson_recommendation1():
+    from nose.tools import eq_
+
+    # データセットの指定
+    import dataset
+    no = 2
+    data = dataset.dataset(no)
+
+    # テストパラメータの設定
+    sim_func_name = "pearson"
+    cf_model = cf(data.Us, data.Is, data.U2I2Rating, sim_func_name)
+
+    # 類似度テスト
+    user1 = "u1"
+    user2 = "u2" # target
+    user3 = "u3"
+    user4 = "u4"
+    eq_(cf_model.get_sim(user2, user1),  0.0)
+    eq_(cf_model.get_sim(user2, user3),  1.0)
+    eq_(cf_model.get_sim(user2, user4), -1.0)
+    
+    # スコアテスト
+    user = "u2"
+    item = "i1"
+    score = cf_model.calc_score(user, item)
+    eq_(score, 2.75)
+    return
+
+def test_pearson_recommendation2():
+    from nose.tools import eq_
+
+    # データセットの指定
+    import dataset
+    no = 3
+    data = dataset.dataset(no)
+
+    # テストパラメータの設定
+    sim_func_name = "pearson"
+    cf_model = cf(data.Us, data.Is, data.U2I2Rating, sim_func_name)
+
+    # 類似度テスト
+    user1 = "u1" # target
+    user2 = "u2"
+    user3 = "u3"
+    user4 = "u4"
+    eq_(cf_model.get_sim(user1, user2), -0.8)
+    eq_(cf_model.get_sim(user1, user3),  1.0)
+    eq_(cf_model.get_sim(user1, user4),  0.0)
+    
+    # スコアテスト
+    user = "u1"
+    item = "i6"
+    score = cf_model.calc_score(user, item)
+    eq_(score, 3.0 + (2-(-0.8)) / (abs(1) + abs(-0.8)) ) # 4.555...
+    return
+
+
         
 if __name__ == "__main__":
     import sys
     import dataset
+    I2Rating1 = {"i1":1}
+    I2Rating2 = {"i1":1}
+    print(cosine_similarity(I2Rating1, I2Rating2))
 
     print('*** begin ***');sys.stdout.flush()
 
     print('=== get dataset ===');sys.stdout.flush()
-    # no = 0
-    # data = dataset(no, N=2000, M=400, K=20, R=5, seed=1)
     no = 3
     data = dataset.dataset(no)
     # print(data)
@@ -162,7 +329,7 @@ if __name__ == "__main__":
             score = cf_model.calc_score(user, item)
             print("     ", item, score)
 
-    print('=== recommend item ===');sys.stdout.flush()
+    print('=== recommend items ===');sys.stdout.flush()
     N = 10
     for user in cf_model.Us:
         RecIs =cf_model.recommend_item(user, N)
@@ -171,10 +338,3 @@ if __name__ == "__main__":
 
     print('***  end  ***');sys.stdout.flush()
 
-    # dataset:no=3
-    # pearson
-    # u1 - i6:score = 4.56?(=4.555555555555555)  
-
-    # dataset:no=2
-    # pearson
-    # u2 - i1:score = 2.75?
